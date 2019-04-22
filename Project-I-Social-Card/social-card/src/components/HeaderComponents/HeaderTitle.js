8143c3e8e9206f148db76f6c0a52e4f284969fa9
victorN <victornguyen082@gmail.com>
2019-04-22 16:31:32 -0400

    Template overlay, header and card contents

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title">
+        <h3>Lambda School</h3>
+        </div>
+    );
+};
+
+export default HeaderTitle;
